bddf84561db2fde5cccf29b295813803eb1eee02
Stompe, Danny <danny.stompe+kpn@kpn.com>
2025-09-21 14:26:19 +0200

    add colloring and table improvements

--- src/render_html.py
+++ src/render_html.py
@@ -12,7 +12,7 @@ def render_html_table(
     if not isinstance(rows, list):
         raise TypeError("rows must be a list of dicts")
 
-    excluded = {"yahoo_details", "signal", "histogram", "last_close", "sma30_value"}
+    excluded = {"yahoo_details", "perform","signal", "histogram", "last_close", "sma30_value"}
 
     # make clickable links for keys containing 'ticker'
     for r in rows:
@@ -46,15 +46,30 @@ def render_html_table(
                     return f"<td class='sma_neg'>{escape(txt)}</td>"
             except ValueError:
                 pass
-
+        # PRICE
         elif "price" in row and "ycp" in row and val == row.get("price"):
             price, ycp = float(row["price"]), float(row["ycp"])
             if price > ycp:
                 return f"<td class='pos_price'>{price}</td>"
             elif price < ycp:
                 return f"<td class='neg_price'>{price}</td>"
-        elif "name" in row and "sector" in row and val == row.get("name"):
-            return f"<td class='name'>{txt}</td>"
+        # NAME  
+        elif "name" in row and val == row.get("name"):
+            if row.get("perform") == "excellent":
+                return f"<td class='name_excellent'>{txt}</td>"
+            elif row.get("perform") == "good":
+                return f"<td class='name_good'>{txt}</td>"
+
+            elif row.get("perform") == "weak":
+                return f"<td class='name_weak'>{txt}</td>"
+            elif row.get("perform") == "poor":
+                return f"<td class='name_poor'>{txt}</td>"
+            else:
+                return f"<td class='name_neutral'>{txt}</td>"
+        # remove perform column. performance is indicated by name color
+        elif "perform" in row and val == row.get("perform"):
+            return ""
+        # SECTOR
         elif "sector" in row and val == row.get("sector"):
             return f"<td class='sector'>{txt}</td>"
         elif "<a href" in lower_txt:
@@ -87,6 +102,7 @@ def render_html_table(
 
         return f"<td>{txt}</td>"
 
+
     # Build rows
     rows_html = []
     for r in rows:
@@ -99,26 +115,39 @@ def render_html_table(
     css = """
     body { font-family:'Segoe UI',Roboto,Helvetica,Arial,sans-serif; padding:6px; background:linear-gradient(135deg,#f0f4f8,#d9e2ec);}
     .card { background:white; border-radius:6px; box-shadow:0 6px 10px rgba(0,0,0,0.1); padding:4px; max-width:85%; margin:1px; overflow-x:auto;}
-    table { border-collapse:collapse; width:100%; border-radius:6px; overflow:hidden;}
+    table { border-collapse:collapse; width:100%; border-radius:6px; overflow:hidden; vertical-align: middle;}
     caption { text-align:center; font-size:1rem; padding-bottom:7px; font-weight:400; color:#243b53;}
     th,td { color:#333332; padding:5px 6px; border-bottom:1px solid #ced9e5; text-align:center; vertical-align:top; font-size:0.7rem;}
     th { background:#f8fafc; color:#334e68; font-weight:400; text-transform:uppercase;}
     tr:hover td { border-bottom: 1px solid #ee6c6c; border-top-color: 1px solid #ee6c6c; transition:background 0.5s ease;}
+    .name_excellent { color:#154330; background-color:#37d29b; text-align:left;}
+    .name_poor { color:#fff; background-color:f05e54; text-align:left;}
+    .name_good { color:#a3dec9; background-color:#4c7868; text-align:left;}
+    .name_weak { color:#593a1a; background-color:#eca865; text-align:left;}
+    .name_neutral { color:#747852; background-color:#ebeb7c; text-align:left;}
     .small { font-size:0.55rem; color:#52667a; margin-top:10px;}
-    .positive { color:#fff; background-color:#709a8b;}
-    .negative { color:#fff; background-color:#ee6c6c;}
-    .small_pos { color:#fff; background-color:#9ccbba;}
-    .small_neg { color:#fff; background-color:#ddac78;}
-    .neutral { color:#35362c; background-color:#f6f6e5;}
-    .ticker { color:#333332; text-align:center; vertical-align: middle; border-radius:16px; font-weight:400; padding:3px 4px; background-color:#f0f4f8;}
+    .positive { color:#154330; background-color:#37d29b;}
+    .negative { color:#482222; background-color:#f05e54;}
+    .small_pos { color:#a3dec9; background-color:#4c7868;}
+    .small_neg { color:#593a1a; background-color:#eca865;}
+    .neutral { color:#747852; background-color:#ebeb7c;}
+    .ticker { color:#fff; text-align:center; vertical-align: middle; border-radius:16px; font-weight:400; padding:3px 4px; background-color:#bec0bf;}
     .neg_price { color:#d64545;}
     .pos_price { color:#709a8b;}
     .sma_neg { color:#d64545;}
-    .sma_pos { color:#709a8b;}
+    .sma_pos { color:#9ccba;}
     .name { text-align:left; font-weight:500; color:#243b53;}
     .sector { text-align:left; font-style:italic; color:#52667a;}
-    .name:hover {cursor:default;}
-    .positive:hover { background-color:#f1f1f1;}
+    a:link,
+    a:visited {
+    color: #fff;   /* or whatever color you prefer */
+    text-decoration: none; /* optional, removes underline */
+    }
+
+    a:hover,
+    a:active {
+    color: #d5dbd9; /* optional hover/active color */
+    }
     @media (max-width:768px){
       th,td{ padding:6px 2px; font-size:0.6rem;}
       caption{ font-size:1.2rem;}
